37f6a46a602c7cc3d34f8a2464bd4494f7a73261
alyciandrade <alyciandrade@gmail.com>
2024-09-17 11:12:38 -0300

    upando o back-end

---
+++ Back-end-P.I-I-main/PI.BackEnd.API/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PI.BackEnd.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginRequest loginRequest)
+        {
+            return Ok();
+        }
+    }
+
+    public class LoginRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Back-end-P.I-I-main/PI.BackEnd.API/Services/AuthService.cs
@@ -0,0 +1,10 @@
+﻿namespace PI.BackEnd.API.Services
+{
+    public class AuthService
+    {
+        public bool ValidateUser(string email, string password)
+        {
+            return true;
+        }
+    }
+}
